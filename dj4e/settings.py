5c2e355bef1d252872c6b3a56fd323b37b13c8e3
niyirerayannick <niyannick120@gmail.com>
2024-01-27 12:18:59 +0200

    chda

--- dj4e/settings.py
+++ dj4e/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+APP_NAME = 'ChucksList'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'autos',
-    'cats',
-    "home",
+    'home',
+    'ads',
+    "social_django",
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
